783a1c3d9aa966c545ab60d2bc17c7c7604395ad
Johnathon Wood <jaywood128@gmail.com>
2018-11-07 20:00:58 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,34 +1,17 @@
 require "pry"
 
 class Book
+  attr_accessor: :author, :page_count, :genre
+
   def initialize(title)
     @title = title
   end
   def title
     @title
   end
-  def author=(author)
-    @author = author
-  end
-  def author
-    @author
-  end
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-  def page_count
-    @page_count
-  end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-  def genre
-    @genre
   end
 end
 
